9df98c60626850e30318e43fc9eb3f4897f62f12
benni90100 <micelibenedetto800@gmail.com>
2024-02-28 20:59:05 +0100

    es2

---
+++ es2.py
@@ -0,0 +1,9 @@
+numero1 = int(input('digita il primo numero :'))
+numero2 = int(input('digita il primo numero :'))
+
+if numero1>numero2:
+    print("il numero maggiore e' ",numero1)
+elif numero2> numero1 :
+    print("il numero maggiore e' ",numero2)
+else:
+    print("i numeri sono uguali")
